b4e44cfc5a3d70eb61a0756ac9e7b2282cc74c3c
Gabriel Casanova <con.casanovaproductions@outlook.com>
2026-04-25 14:42:13 -0300

    Terminada ad2

--- ad2/cpw_gbcasanova/script.js
+++ ad2/cpw_gbcasanova/script.js
@@ -52,5 +52,75 @@ function mudarDescricao(nomeDoProduto) {
     }
 }
 
+// CPF
+function calculaDV(num) {
+    var resto = 0, soma = 0;
+    for (i = 2; i < 11; i++) {
+        soma = soma + ((num % 10) * i);
+        num = parseInt(num / 10);
+    }
+    resto = (soma % 11);
+    return (resto > 1) ? (11 - resto) : 0;
+}
 
+function validarCPF() {
+    var cpfInput = document.getElementById("cpf").value;
 
+    if (isNaN(cpfInput)) {
+        alert("O CPF deve ter apenas números");
+        return;
+    }
+
+    if (cpfInput.length !== 11) {
+        alert("O CPF deve ter 11 dígitos!");
+        return;
+    }
+
+    var identCPF = parseInt(cpfInput.substring(0, 9));
+    
+    var dvDigitado = parseInt(cpfInput.substring(9, 11));
+
+    var primeiro_digito = calculaDV(identCPF);
+    var segundo_digito = calculaDV(identCPF * 10 + primeiro_digito);
+    
+    var dvCalculado = (primeiro_digito * 10) + segundo_digito;
+
+    if (dvDigitado !== dvCalculado) {
+        alert("Dígitos verificadores incorretos!");
+        return;
+    }
+}
+
+// Adicionar produto
+const precosProdutos = {
+    "pedigree": 100.00,
+    "purina": 130.00,
+    "granplus": 130.00,
+    "jambo": 25.00
+};
+
+function adicionarProduto() {
+    var combo = document.getElementById("produtos");
+    var lista = document.getElementById("listaCompras");
+    var campoValor = document.getElementById("valor");
+
+    if (combo.value === "") {
+        alert("Nnehum produto selecionado!");
+        return;
+    }
+
+    var nomeProduto = combo.options[combo.selectedIndex].text;
+    var valorProduto = precosProdutos[combo.value];
+
+    if (lista.value !== "") {
+        lista.value += "\n" + nomeProduto;
+    } else {
+        lista.value = nomeProduto;
+    }
+
+    var valorAtual = parseFloat(campoValor.value);
+    var novoValorTotal = valorAtual + valorProduto;
+    campoValor.value = novoValorTotal.toFixed(2); 
+    
+    combo.selectedIndex = 0;
+}
