1227c428cb1a43a1584b66129ef6322e5712385b
Gabriel Casanova <con.casanovaproductions@outlook.com>
2026-04-25 11:14:42 -0300

    Site e trabalhos AD2 atualizados

---
+++ ad2/cpw_gbcasanova/script.js
@@ -0,0 +1,7 @@
+function abrirJanela(idDoModal) {
+    document.getElementById(idDoModal).style.display = 'block';
+}
+
+function fecharJanela(idDoModal) {
+    document.getElementById(idDoModal).style.display = 'none';
+}
